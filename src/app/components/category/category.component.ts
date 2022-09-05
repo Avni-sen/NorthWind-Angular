358e419f19a715f824579ed13e92a9a808ca722b
Avni Sen <senavni35@gmail.com>
2022-09-05 13:42:32 +0300

    first commit

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+
+
+  Categories: Category[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
